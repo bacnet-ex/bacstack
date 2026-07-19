0d2fa3775ca66f8fff52e6139dc14c4b7e0a8a2c
CI <ci@git.local>
2026-07-19 14:08:46 +0000

    Sync to GitHub

--- dialyzer.ignore-warnings.exs
+++ dialyzer.ignore-warnings.exs
@@ -1,4 +1,29 @@
 [
   # Cause is not consolidating protocols in :dev environment
-  ~r/unknown_function.*__impl__.*/
+  ~r/unknown_function.*__impl__.*/,
+  # These functions raise and Dialyzer in OTP29 complains about that (Dialyzer resolves to `none()` return type)
+  {"lib/bacnet/protocol/apdu/abort.ex",
+   "Invalid type specification for function encode_segmented."},
+  {"lib/bacnet/protocol/apdu/abort.ex",
+   "Invalid type specification for function encode_to_segmented."},
+  {"lib/bacnet/protocol/apdu/error.ex",
+   "Invalid type specification for function encode_segmented."},
+  {"lib/bacnet/protocol/apdu/error.ex",
+   "Invalid type specification for function encode_to_segmented."},
+  {"lib/bacnet/protocol/apdu/reject.ex",
+   "Invalid type specification for function encode_segmented."},
+  {"lib/bacnet/protocol/apdu/reject.ex",
+   "Invalid type specification for function encode_to_segmented."},
+  {"lib/bacnet/protocol/apdu/segment_ack.ex",
+   "Invalid type specification for function encode_segmented."},
+  {"lib/bacnet/protocol/apdu/segment_ack.ex",
+   "Invalid type specification for function encode_to_segmented."},
+  {"lib/bacnet/protocol/apdu/simple_ack.ex",
+   "Invalid type specification for function encode_segmented."},
+  {"lib/bacnet/protocol/apdu/simple_ack.ex",
+   "Invalid type specification for function encode_to_segmented."},
+  {"lib/bacnet/protocol/apdu/unconfirmed_service_request.ex",
+   "Invalid type specification for function encode_segmented."},
+  {"lib/bacnet/protocol/apdu/unconfirmed_service_request.ex",
+   "Invalid type specification for function encode_to_segmented."}
 ]
